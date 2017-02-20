fa2797658156ef5bf5977e50c46d5c18692be066
ch1bi <maine1191@gmail.com>
2017-02-20 17:13:27 -0500

    added code to find indexes in ascending order

--- src/consec/consecutiveTest.java
+++ src/consec/consecutiveTest.java
@@ -11,10 +11,10 @@ public class consecutiveTest {
 		Scanner scanner = new Scanner(System.in);
 		
 		//create an array to store user values
-		int myArray[] = new int[5];
+		int myArray[] = new int[11];
 		
 		
-		System.out.println("Enter 5 numbers");
+		System.out.println("Enter 11 numbers");
 		
 		//create loop to populate array
 		for(int i = 0; i < myArray.length; i++){
@@ -37,41 +37,25 @@ public class consecutiveTest {
 		
 		
 		//use a loop to find consecutive nums and add to list
-		for(int i = 0; i < array.length; i++){
+		for(int i = 0; i < array.length-1; i++){
 			
-			if(i <= 0 && i <= array.length)
-			{
-				//if i > current position +1 and current position +2
-				if((array[i] > array[i+1]) && (array[i]) > array[i+2]) {
+				
+				//if array position +1 equals current value of array +1 ... - gets indexes forward
+				if((array[i+1] == array[i] + 1) && (array[i+2] == array[i] + 2)){
 					
+					list.add(i);
 					
-					
-					
+						
 				}
 				
-				else{
-					
-					//if i is not less than current position +1 and +2 break out of current iteration
-					break;	
-					
-				}
 			
 			}
 			
-			else{
-				
-				//break out loop if is out of bounds
-				continue;
-			}
-				
 			
-		}
-		
-
-			
-			//create a new array to return values back to calling method
+			//create a new array equal to the collected list of indexes
 			int[] newArr = new int[list.size()];
 			
+			//loops through newArr to populate it with the lists values of collected indexes
 			for(int j = 0; j < list.size(); j++){
 				
 				newArr[j] = list.get(j);
@@ -79,7 +63,7 @@ public class consecutiveTest {
 			
 		
 		
-		
+		//return the array values as a string1
 		return Arrays.toString(newArr);
 	}
 
