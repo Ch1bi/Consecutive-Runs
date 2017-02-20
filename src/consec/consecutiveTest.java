bb5b414a574c9a31e028d55ede101cc148303f58
ch1bi <maine1191@gmail.com>
2017-02-20 11:37:18 -0500

    added new code to find consec. nums methods

--- src/consec/consecutiveTest.java
+++ src/consec/consecutiveTest.java
@@ -11,10 +11,10 @@ public class consecutiveTest {
 		Scanner scanner = new Scanner(System.in);
 		
 		//create an array to store user values
-		int myArray[] = new int[10];
+		int myArray[] = new int[5];
 		
 		
-		System.out.println("Enter 10 numbers");
+		System.out.println("Enter 5 numbers");
 		
 		//create loop to populate array
 		for(int i = 0; i < myArray.length; i++){
@@ -31,17 +31,43 @@ public class consecutiveTest {
 	
 	public static String findConsecutiveRuns(int[] array){
 		
-		
-		//create a list to store new array values for easier manipulation
+	
+		//create a list to store new array since am array can't increase size
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		
-		//use a loop to find consecutive nums and add start to list
+		
+		//use a loop to find consecutive nums and add to list
 		for(int i = 0; i < array.length; i++){
 			
-
+			if(i <= 0 && i <= array.length)
+			{
+				//if i > current position +1 and current position +2
+				if((array[i] > array[i+1]) && (array[i]) > array[i+2]) {
+					
+					
+					
+					
+				}
 				
-				list.add(array[i]);
+				else{
+					
+					//if i is not less than current position +1 and +2 break out of current iteration
+					break;	
+					
+				}
+			
+			}
+			
+			else{
+				
+				//break out loop if is out of bounds
+				continue;
+			}
+				
+			
 		}
+		
+
 			
 			//create a new array to return values back to calling method
 			int[] newArr = new int[list.size()];
@@ -49,11 +75,6 @@ public class consecutiveTest {
 			for(int j = 0; j < list.size(); j++){
 				
 				newArr[j] = list.get(j);
-			}
-			
-			for(int i : newArr){
-				
-				System.out.println(i);
 			}
 			
 		
